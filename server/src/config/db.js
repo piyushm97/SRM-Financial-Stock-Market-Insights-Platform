1ece57476bda116c2b20669c7b47f8d97caf6b65
Piyush Mahajan <68603411+piyushm97@users.noreply.github.com>
2025-12-05 10:21:26 -0600

    Create db.js

---
+++ server/src/config/db.js
@@ -0,0 +1,12 @@
+// server/src/config/db.js
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/srm_financial";
+
+mongoose
+  .connect(uri, { serverSelectionTimeoutMS: 5000 })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err.message));
