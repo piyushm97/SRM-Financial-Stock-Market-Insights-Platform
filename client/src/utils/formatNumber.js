e26753fd17d146c64fddf7c72579c731154df0f2
Piyush Mahajan <68603411+piyushm97@users.noreply.github.com>
2025-12-30 09:26:38 -0600

    Create formatNumber.js

---
+++ client/src/utils/formatNumber.js
@@ -0,0 +1,7 @@
+// client/src/utils/formatNumber.js
+export function formatNumber(value, decimals = 2) {
+  return Number(value).toLocaleString("en-US", {
+    minimumFractionDigits: decimals,
+    maximumFractionDigits: decimals
+  });
+}
